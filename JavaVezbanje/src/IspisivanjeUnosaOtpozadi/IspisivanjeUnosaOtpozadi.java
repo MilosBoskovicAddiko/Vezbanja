094087b405a93ca80eb6292e874aeab057aa82e6
Milos Boskovic <milosboskovicaddiko@gmail.com>
2019-07-18 10:39:52 +0200

    Refactoring IspisivanjeUnosaOdPozadi

--- JavaVezbanje/src/IspisivanjeUnosaOtpozadi/IspisivanjeUnosaOtpozadi.java
+++ JavaVezbanje/src/IspisivanjeUnosaOtpozadi/IspisivanjeUnosaOtpozadi.java
@@ -6,9 +6,11 @@ public class IspisivanjeUnosaOtpozadi {
     public static void main(String[] args) {
         System.out.print("Unesite neki tekst: ");
         Scanner skener = new Scanner(System.in);
-        StringBuilder sbld = new StringBuilder();
         String unos = skener.nextLine();
-        sbld.append(unos);
-        System.out.print("Uneti tekst ispisan otpozadi glasi: "+sbld.reverse());
+        String obrnutiUnos="";
+        for(int i = unos.length()-1; i >= 0; i--){
+            obrnutiUnos+=unos.charAt(i);
+        }
+        System.out.print("Uneti tekst ispisan otpozadi glasi: "+obrnutiUnos);
     }
 }
