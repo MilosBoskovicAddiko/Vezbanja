0687631890908a01428ce707017c95f3a5a26fad
Milos Boskovic <milosboskovicaddiko@gmail.com>
2019-08-01 15:38:54 +0200

    Papir - kamen - makaze - NOVO

--- JavaVezbanje/src/PapirKamenMakaze/KamenPapirMakaze.java
+++ JavaVezbanje/src/PapirKamenMakaze/KamenPapirMakaze.java
@@ -6,195 +6,74 @@ import java.util.Scanner;
 public class KamenPapirMakaze {
 
     public static void main(String[] args) {
-        int pointsHuman = 0;
-        int pointsComputer = 0;
-        String player = "";
-        String computer = "";
+        int poeniCovek = 0;
+        int poeniMasina = 0;
+        String covek = "";
+        String masina = "";
+
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Dobrodosli u igru \"Kamen-Papir-Makaze\"");
         System.out.println("-----------------------------------------------------------------");
         System.out.println("Moguce opcije su: ");
         System.out.println("1. Kamen\n2. Papir\n3. Makaze");
         System.out.println("Igru u svakom trenutku mozete prekinuti unoseci rec \"kraj\"");
         System.out.println("-----------------------------------------------------------------");
-        while ((pointsHuman < 21 || pointsComputer < 21) || (pointsHuman >= 21 && pointsComputer >= 21 && pointsComputer == pointsHuman)) {
-            System.out.println("Unesite zeljenu rec: ");
-            player = scanner.nextLine();
-            if (player.equalsIgnoreCase("kraj")) {
-                System.out.println("Igra je zavrsena!!!");
-                System.exit(0);
-            }
-            while (!player.equalsIgnoreCase("papir") && !player.equalsIgnoreCase("kamen") && !player.equalsIgnoreCase("makaze")) {
-                System.out.println("Unesite zeljenu rec: ");
-                player = scanner.nextLine();
-                if (player.equalsIgnoreCase("kraj")) {
-                    System.out.println("Igra je zavrsena!!!");
-                    System.exit(0);
-                }
-            }
-            int compChoice = rand.nextInt(3);
-            switch (compChoice) {
-                case 0:
-                    computer = "kamen";
-                    break;
-                case 1:
-                    computer = "papir";
-                    break;
-                case 2:
-                    computer = "makaze";
-                    break;
-            }
 
-            System.out.println("Racunar je izabrao: " + computer);
-
-            if (player.equalsIgnoreCase(computer)) {
-                pointsHuman++;
-                pointsComputer++;
-            } else if (player.equalsIgnoreCase("kamen") && computer.equalsIgnoreCase("makaze")) {
-                pointsHuman = pointsHuman + 2;
-            } else if (player.equalsIgnoreCase("kamen") && computer.equalsIgnoreCase("papir")) {
-                pointsComputer = pointsComputer + 2;
-            } else if (player.equalsIgnoreCase("papir") && computer.equalsIgnoreCase("kamen")) {
-                pointsHuman = pointsHuman + 2;
-            } else if (player.equalsIgnoreCase("papir") && computer.equalsIgnoreCase("makaze")) {
-                pointsComputer = pointsComputer + 2;
-            } else if (player.equalsIgnoreCase("makaze") && computer.equalsIgnoreCase("kamen")) {
-                pointsComputer = pointsComputer + 2;
+        while (poeniCovek < 21 || poeniMasina < 21 || (poeniCovek >= 21 && poeniMasina >= 21)) {
+            if (poeniMasina >= 21 && poeniMasina > poeniCovek) {
+                System.out.println("Pobednik je kompijuter!");
+                break;
+            } else if (poeniCovek >= 21 && poeniCovek > poeniMasina) {
+                System.out.println("Pobednik je covek!");
+                break;
             } else {
-                pointsHuman = pointsHuman + 2;
-            }
+                System.out.print("Unesite zeljenu rec: ");
+                covek = scanner.nextLine();
 
-            System.out.println("Trenutni broj poena: ");
-            System.out.println("Covek: " + pointsHuman);
-            System.out.println("Masina: " + pointsComputer);
-            System.out.println("-----------------------------------------------------------------");
-            if (pointsComputer != pointsHuman && (pointsHuman >= 21 || pointsComputer >= 21)) {
-                if (pointsComputer > pointsHuman) {
-                    System.out.println("Pobednik je kompijuter.");
-                    System.exit(0);
-                } else {
-                    System.out.println("Cestitamo, vi ste pobedili.");
-                    System.exit(0);
+                if (covek.equalsIgnoreCase("kraj")) {
+                    break;
+                }
+                else if (covek.equalsIgnoreCase("kamen") || covek.equalsIgnoreCase("papir") || covek.equalsIgnoreCase("makaze")) {
+                    int compChoice = rand.nextInt(3);
+                    switch (compChoice) {
+                        case 0:
+                            masina = "kamen";
+                            break;
+                        case 1:
+                            masina = "papir";
+                            break;
+                        case 2:
+                            masina = "makaze";
+                            break;
+                    }
+
+                    System.out.println("Racunar je izabrao: " + masina);
+
+                    if (covek.equalsIgnoreCase(masina)) {
+                        poeniCovek++;
+                        poeniMasina++;
+                    } else if (covek.equalsIgnoreCase("kamen") && masina.equalsIgnoreCase("makaze")) {
+                        poeniCovek = poeniCovek + 2;
+                    } else if (covek.equalsIgnoreCase("kamen") && masina.equalsIgnoreCase("papir")) {
+                        poeniMasina = poeniMasina + 2;
+                    } else if (covek.equalsIgnoreCase("papir") && masina.equalsIgnoreCase("kamen")) {
+                        poeniCovek = poeniCovek + 2;
+                    } else if (covek.equalsIgnoreCase("papir") && masina.equalsIgnoreCase("makaze")) {
+                        poeniMasina = poeniMasina + 2;
+                    } else if (covek.equalsIgnoreCase("makaze") && masina.equalsIgnoreCase("kamen")) {
+                        poeniMasina = poeniMasina + 2;
+                    } else {
+                        poeniCovek = poeniCovek + 2;
+                    }
+
+                    System.out.println("Trenutni broj poena: ");
+                    System.out.println("Covek: " + poeniCovek);
+                    System.out.println("Masina: " + poeniMasina);
+                    System.out.println("-----------------------------------------------------------------");
                 }
             }
         }
-    }
-
-    public static boolean isParametersEnteredCorrectly(String playerChoice, int computerChoice) {
-        int pointsHuman = 0;
-        int pointsComputer = 0;
-        String player = playerChoice;
-        String computer = "";
-        System.out.println("Dobrodosli u igru \"Kamen-Papir-Makaze\"");
-        System.out.println("-----------------------------------------------------------------");
-        System.out.println("Moguce opcije su: ");
-        System.out.println("1. Kamen\n2. Papir\n3. Makaze");
-        System.out.println("Igru u svakom trenutku mozete prekinuti unoseci rec \"kraj\"");
-        System.out.println("-----------------------------------------------------------------");
-        while ((pointsHuman < 21 || pointsComputer < 21) || (pointsHuman >= 21 && pointsComputer >= 21 && pointsComputer == pointsHuman)) {
-            if (player.equalsIgnoreCase("kraj")) {
-                System.out.println("Igra je zavrsena!!!");
-                System.exit(0);
-                return true;
-            }
-            while (!player.equalsIgnoreCase("papir") && !player.equalsIgnoreCase("kamen") && !player.equalsIgnoreCase("makaze")) {
-                if (player.equalsIgnoreCase("kraj")) {
-                    System.out.println("Igra je zavrsena!!!");
-                    System.exit(0);
-                    return true;
-                }
-                return false;
-            }
-            int compChoice = computerChoice;
-            switch (compChoice) {
-                case 0:
-                    computer = "kamen";
-                    break;
-                case 1:
-                    computer = "papir";
-                    break;
-                case 2:
-                    computer = "makaze";
-                    break;
-            }
-
-            if (player.equalsIgnoreCase(computer)) {
-                pointsHuman++;
-                pointsComputer++;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return false;
-            } else if (player.equalsIgnoreCase("kamen") && computer.equalsIgnoreCase("makaze")) {
-                pointsHuman = pointsHuman + 2;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return true;
-            } else if (player.equalsIgnoreCase("kamen") && computer.equalsIgnoreCase("papir")) {
-                pointsComputer = pointsComputer + 2;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return false;
-            } else if (player.equalsIgnoreCase("papir") && computer.equalsIgnoreCase("kamen")) {
-                pointsHuman = pointsHuman + 2;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return true;
-            } else if (player.equalsIgnoreCase("papir") && computer.equalsIgnoreCase("makaze")) {
-                pointsComputer = pointsComputer + 2;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return false;
-            } else if (player.equalsIgnoreCase("makaze") && computer.equalsIgnoreCase("kamen")) {
-                pointsComputer = pointsComputer + 2;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return false;
-            } else {
-                pointsHuman = pointsHuman + 2;
-                System.out.println("Covek bira: " + player);
-                System.out.println("Masina bira: " + computer);
-                System.out.println("Trenutni broj poena: ");
-                System.out.println("Covek: " + pointsHuman);
-                System.out.println("Masina: " + pointsComputer);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean isWinnerValid(int pointsHuman, int pointsComputer) {
-        System.out.println("Trenutni broj poena: ");
-        System.out.println("Covek: " + pointsHuman);
-        System.out.println("Masina: " + pointsComputer);
-        System.out.println("-----------------------------------------------------------------");
-        if (pointsComputer != pointsHuman && (pointsHuman >= 21 || pointsComputer >= 21)) {
-            if (pointsComputer > pointsHuman) {
-                System.out.println("Pobednik je kompijuter.");
-                return false;
-            } else {
-                System.out.println("Cestitamo, vi ste pobedili.");
-                return true;
-            }
-        }
-        return false;
     }
 }
