30badcf2d495c6996cd11734207c19c2d6de7dcb
Milos Boskovic <milosboskovicaddiko@gmail.com>
2019-08-06 12:14:14 +0200

    Jumper - Refactoring

--- JavaVezbanje/src/SkockoSlagalica/PossibleChoices.java
+++ JavaVezbanje/src/SkockoSlagalica/PossibleChoices.java
@@ -1,20 +1,10 @@
 package SkockoSlagalica;
 
 public enum PossibleChoices {
-    SKOCKO(1),
-    ZVEZDA(2),
-    KARO(3),
-    PIK(4),
-    HERC(5),
-    TREF(6);
-
-    private final int id;
-
-    PossibleChoices(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
+    SKOCKO,
+    ZVEZDA,
+    KARO,
+    PIK,
+    HERC,
+    TREF
 }
