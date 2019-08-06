5230a128269ab1be88393d927f5ab4a9ddbecebf
Milos Boskovic <milosboskovicaddiko@gmail.com>
2019-08-06 15:54:39 +0200

    Factoriel

--- JavaVezbanje/src/Factoriel/FactorielExample.java
+++ JavaVezbanje/src/Factoriel/FactorielExample.java
@@ -6,21 +6,16 @@ public class FactorielExample {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        boolean flag = true;
-        while (flag){
-            System.out.print("Enter some integer: ");
-            String input = scanner.nextLine();
-            try {
-                int number = Integer.parseInt(input);
-                long fact = factoriel(number);
-                System.out.println(number + "! = " + fact);
-                flag = false;
+        System.out.print("Enter some integer: ");
+        String input = scanner.nextLine();
+        try {
+            int number = Integer.parseInt(input);
+            long fact = factoriel(number);
+            System.out.println(number + "! = " + fact);
 
-            } catch (NumberFormatException nfe) {
-                System.out.println("Your input is not valid!!!" + nfe.getMessage());
-            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("Your input is not valid!!!" + nfe.getMessage());
         }
-
     }
 
     public static long factoriel(int number) {
